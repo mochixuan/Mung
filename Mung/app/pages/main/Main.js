09fcb42e1e09740e2e3b3bfe9b792e96c24a9d5d
wangxuan <1306894924@qq.com>
2017-09-04 08:45:33 +0800

    base

--- Mung/app/pages/main/Main.js
+++ Mung/app/pages/main/Main.js
@@ -4,7 +4,7 @@ import {
     Text,
 } from 'react-native'
 
-import {Tabs,Tab,Icon} from 'react-native-elements'
+
 import {show} from "../../utils/ToastUtils"
 import {MainColor,GrayColor,GrayWhiteColor} from '../base/BaseStyle'
 import Movie from '../movie/Movie'
@@ -13,67 +13,14 @@ export default class Main extends Component {
 
     constructor(props) {
         super(props)
-        this.state = {
-            selectedTab: 0,
-        }
     }
 
     render() {
         return (
-            <Tabs>
-                <Tab
-                    titleStyle={{
-                        color: {GrayColor},
-                        fontWeight: 'bold',
-                        fontSize: 10,
-                    }}
-                    selectedTitleStyle={{
-                        color: {MainColor},
-                        fontWeight: 'bold',
-                        fontSize: 10,
-                    }}
-                    selected={
-                        this.state.selectedTab==0
-                    }
-                    title= 'Movie'
-                    renderIcon={()=>{
-                        return <Icon
-                            containerStyle={{
-                                justifyContent: 'center',
-                                alignItems:'center',
-                                marginTop:12
-                            }}
-                            color= {GrayColor}
-                            name={require('../../data/img/ic_tab_movie.png')}
-                            size={33}
-                        />
-                    }}
-                    renderSelectedIcon={()=>{
-                        return <Icon
-                            containerStyle={{
-                                justifyContent: 'center',
-                                alignItems:'center',
-                                marginTop:12
-                            }}
-                            color= {MainColor}
-                            name={require('../../data/img/ic_tab_movie.png')}
-                            size={33}
-                        />
-                    }}
-                    onPress={()=>{
-                        this.changeTab(0)
-                    }}
-                >
-                    <Movie/>
-                </Tab>
-            </Tabs>
+            <Text>a</Text>
         )
     }
 
-    changeTab(position) {
-        this.setState({
-            selectedTab:position
-        })
-    }
+
 
 }
