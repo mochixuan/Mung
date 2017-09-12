641371acdc08d8631231a853314870757b945d64
wangxuan <1306894924@qq.com>
2017-09-12 09:01:51 +0800

    base

--- Mung/app/data/constant/BaseContant.js
+++ Mung/app/data/constant/BaseContant.js
@@ -3,17 +3,23 @@ const Init_HotMovies = {
     count:5,
     start:0,
     total: 1,
-    "subjects":[{
-
-    },{
-
-    },{
-
-    },{
-
-    },{
-
-    }]
+    "subjects":{
+        count:20,
+        start:-1,
+        total: 0,
+        subjects:[{
+           rating:{
+               average: 8,
+           },
+            genres: [
+                "动作","科幻","爱情"
+            ],
+            title: "蓝豆",
+            casts:[{name:"mung"},{name:"mung"},{name:"mung"}],
+            directors:[{name:"mochixuan",avatars:{large:require("../img/icon_default_icon.png")}}],
+            images:{large:require("../img/icon_default_cover.png")}
+        }]
+    }
 }
 
 export {App_Name}
